ca9687385a4e3f0790922b2983ccadd483facb46
DJTheCreator <diegocajiao@outlook.com>
2023-11-07 22:38:57 -0500

    -Outputs to new file -Patched occasional title line

--- Split Data To Columns.py
+++ Split Data To Columns.py
@@ -1,5 +1,4 @@
-use_needed_columns = False
-needed_columns = (0, 4, 5)
+needed_columns = (0, 4, 5, 6)
 
 with open('hurricane dataset.txt') as f:
     lines = f.readlines()
@@ -12,16 +11,15 @@ for line in lines:
     for i in line:
         if i == ',':
             column = line[0:line.find(',')]
-            if k in needed_columns and use_needed_columns:
+            if k in needed_columns:
                 storm.append(column.strip())
-            elif not use_needed_columns:
-                storm.append(column.strip())
+            #print(k in needed_columns)
             line = line[line.find(',')+1:]
             k += 1
     if len(storm) > 3:
         hurricaneList.append(storm)
+# for i in range(len(hurricaneList)):
+#     print(hurricaneList[i])
+f = open("Filtered_Hurricane_Dataset.txt", "w+")
 for i in range(len(hurricaneList)):
-    print(hurricaneList[i])
-
-#Current Issue, data still includes occasional storm Name
-#AL092022, IAN, 40
+    f.write(str(hurricaneList[i]) + "\n")
